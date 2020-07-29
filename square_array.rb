6eb9eb22ade6ff887b5a05435eb45665dfc5a40e
Nick Bazaldua <bazalnick@gmail.com>
2020-07-29 22:46:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+square_array = [1, 2, 3]
+new_array = []
+square_array.each do |numbers|
+  new_array = numbers ** 2 
+  square_array << new_array
+  end
 end
